57a25c930239d8b67dc3ed6103640148c43244b5
Sylvia <sfglamlife@gmail.com>
2020-07-27 21:03:11 -0400

    updated homework

--- dom.js
+++ dom.js
@@ -1,29 +1,13 @@
-var secondItem = document.getElementById("Inventory").children[2].className = "cosmetics";
+var secondItem = document.getElementById("Inventory").children[1].className = "cosmetics";
+
+var lastItem = document.getElementById("item6").nextSibling.innerHTML;
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//1) In your "week5" repo, in your practice branch, create a new "index.html" and a "dom.js". Make sure your index.html has everything its suppose to in the head, and that your js is linked too.
-//2) In your index.html create a list with at least 3 items under it.
-//3) use JS to change the styles of each of the elements in your list (can be as basic as .color).
-//4) Change the class name of your second item in your list using child (in js).
-//5) Change the text value of your last item in your list using sibling (in js).
 //6) Create a second list with a list inside of it (nested list). Each list should have at least 2 items. Add a button to the bottom of the page (make sure it's not in a list).
 //7) Create an event listener that is listening for the button click. When the event is triggered, have one the elements in the innermost list change it's display to hidden.
 //8) Look at how the event is propagating through the lists/body of your HTML. Does it make sense to do this in another way?
+//9) git add, commit, push, merge pr
